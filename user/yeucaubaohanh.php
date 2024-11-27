0ed4a443a833a3ffe1514ace3d33a260278491fe
Junsb11 <lt25121999@gmail.com>
2024-11-27 19:54:35 +0700

    cập nhật mới nhất

--- user/yeucaubaohanh.php
+++ user/yeucaubaohanh.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 if (!isset($_SESSION)) {
     session_start();
 }
@@ -6,16 +6,31 @@ if (!isset($_SESSION['tendn'])) {
     header("Location: dangnhap.php");
     exit();
 }
-include "./inc/header.php"; 
-include "./inc/navbar.php"; 
+
+include "./inc/header.php";
+include "./inc/navbar.php";
+include '../admin/connect.php';
+
+$tendn = $_SESSION['tendn'];
+$sql_dn = "SELECT * FROM tai_khoan WHERE TenDangNhap='$tendn'";
+$result_dn = mysqli_query($conn, $sql_dn);
+$data = mysqli_fetch_array($result_dn);
+
+// Kiểm tra đơn hàng của khách hàng
+$order_details = ""; // Lấy các chi tiết đơn hàng từ giỏ hàng nếu cần
+if (isset($_SESSION['cart']) && count($_SESSION['cart']) > 0) {
+    foreach ($_SESSION['cart'] as $item) {
+        $order_details .= $item['tensp'] . " x " . $item['sl'] . " | ";
+    }
+}
 ?>
 
 <!-- Page Header Start -->
 <div class="container-fluid bg-secondary mb-5">
-    <div class="d-flex flex-column align-items-center justify-content-center" style="min-height: 150px">
-        <h1 class="font-weight-semi-bold text-uppercase mb-3">Yêu Cầu Bảo Hành</h1>
+    <div class="d-flex flex-column align-items-center justify-content-center" style="min-height: 300px">
+        <h1 class="font-weight-semi-bold text-uppercase mb-3">Yêu cầu bảo hành</h1>
         <div class="d-inline-flex">
-            <p class="m-0"><a href="home.php">Home</a></p>
+            <p class="m-0"><a href="">Trang chủ</a></p>
             <p class="m-0 px-2">-</p>
             <p class="m-0">Yêu cầu bảo hành</p>
         </div>
@@ -23,69 +38,83 @@ include "./inc/navbar.php";
 </div>
 <!-- Page Header End -->
 
-<!-- Warranty Start -->
-<div class="container-fluid pt-5">
-    <div class="row px-xl-5">
-        <div class="col-lg-8 table-responsive mb-5">
-            <?php 
-            // Hiển thị các sản phẩm yêu cầu bảo hành
-            if (isset($_SESSION['warranty']) && count($_SESSION['warranty']) > 0) { ?>
-                <table class="table table-bordered text-center mb-0">
-                    <thead class="bg-secondary text-dark">
-                        <tr>
-                            <th>Sản phẩm</th>
-                            <th>Lý do bảo hành</th>
-                            <th>Xóa</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <?php foreach ($_SESSION['warranty'] as $item) { ?>
-                            <tr>
-                                <td class="align-middle">
-                                    <img src='<?php echo "../admin/" . $item['hinhanh']; ?>' alt="" style="width: 50px;"> 
-                                    <?php echo $item['tensp']; ?>
-                                </td>
-                                <td class="align-middle"><?php echo htmlspecialchars($item['reason']); ?></td>
-                                <td class="align-middle">
-                                    <a href="deletewarranty.php?id=<?php echo $item['idsp']; ?>" class="btn btn-sm btn-primary">
-                                        <i class="fa fa-times"></i>
-                                    </a>
-                                </td>
-                            </tr>
+<!-- Warranty Request Start -->
+<form method="post" action="process_warranty.php">
+    <div class="container-fluid pt-5">
+        <div class="row px-xl-5">
+            <div class="col-lg-8">
+                <div class="mb-4">
+                    <h4 class="font-weight-semi-bold mb-4">Thông tin khách hàng</h4>
+                    <div class="row">
+                        <div class="col-md-6 form-group">
+                            <label>Họ và Tên</label>
+                            <input class="form-control" name="txthoten" type="text" value='<?php echo $data['HoTen']; ?>' readonly>
+                        </div>
+                        <div class="col-md-6 form-group">
+                            <label>Số điện thoại</label>
+                            <input class="form-control" name="txtsdt" type="text" value='<?php echo $data['SDT']; ?>' readonly>
+                        </div>
+                        <div class="col-md-6 form-group">
+                            <label>E-mail</label>
+                            <input class="form-control" name="txtemail" type="text" value='<?php echo $data['Email']; ?>' readonly>
+                        </div>
+                        <div class="col-md-6 form-group">
+                            <label>Địa chỉ giao hàng</label>
+                            <input class="form-control" name="txtdiachi" type="text" value='<?php echo $data['DiaChi']; ?>' readonly>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="mb-4">
+                    <h4 class="font-weight-semi-bold mb-4">Thông tin bảo hành</h4>
+                    <div class="row">
+                        <div class="col-md-12 form-group">
+                            <label>Tên dòng máy</label>
+                            <input class="form-control" name="txtproduct" type="text" placeholder="Tên dòng máy" required>
+                        </div>
+                        <div class="col-md-12 form-group">
+                            <label>Mã đơn hàng</label>
+                            <input class="form-control" name="txtorder" type="text" value="<?php echo uniqid('ORD'); ?>" readonly>
+                        </div>
+                        <div class="col-md-12 form-group">
+                            <label>Lý do bảo hành</label>
+                            <textarea class="form-control" name="txtreason" placeholder="Vui lòng mô tả lý do bảo hành" required></textarea>
+                        </div>
+                        <div class="col-md-12 form-group">
+                            <label>Ngày yêu cầu bảo hành</label>
+                            <input class="form-control" name="txtdate" type="date" value="<?php echo date('Y-m-d'); ?>" required>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-lg-4">
+                <div class="card border-secondary mb-5">
+                    <div class="card-header bg-secondary border-0">
+                        <h4 class="font-weight-semi-bold m-0">Chi tiết đơn hàng</h4>
+                    </div>
+                    <div class="card-body">
+                        <h5 class="font-weight-medium mb-3">Sản phẩm đã mua</h5>
+                        <?php foreach ($_SESSION['cart'] as $ds) { ?>
+                            <div class="d-flex justify-content-between">
+                                <p><?php echo $ds['tensp'] . ' x ' . $ds['sl']; ?></p>
+                                <p><?php echo number_format($ds['dongia'], 0, '.', '.'); ?></p>
+                            </div>
                         <?php } ?>
-                    </tbody>
-                </table>
-            <?php } else { ?>
-                <p class="text-center">Không có sản phẩm nào trong danh sách bảo hành.</p>
-            <?php } ?>
-        </div>
-
-        <!-- Form yêu cầu bảo hành -->
-        <div class="col-lg-4">
-            <form method="POST" action="submit_warranty_request.php">
-                <div class="form-group">
-                    <label for="product_id">Chọn sản phẩm yêu cầu bảo hành</label>
-                    <select class="form-control" id="product_id" name="product_id" required>
-                        <?php
-                        if (isset($_SESSION['warranty']) && count($_SESSION['warranty']) > 0) {
-                            foreach ($_SESSION['warranty'] as $item) {
-                                echo "<option value='" . $item['idsp'] . "'>" . $item['tensp'] . "</option>";
-                            }
-                        }
-                        ?>
-                    </select>
+                        <hr class="mt-0">
+                    </div>
                 </div>
-
-                <div class="form-group">
-                    <label for="reason">Lý do yêu cầu bảo hành</label>
-                    <textarea class="form-control" id="reason" name="reason" rows="4" required></textarea>
+                <div class="card-footer border-secondary bg-transparent">
+                    <button class="btn btn-lg btn-block btn-primary font-weight-bold my-3 py-3" name="btnsubmit_warranty">Gửi yêu cầu bảo hành</button>
                 </div>
-
-                <button type="submit" class="btn btn-primary btn-block mt-3">Gửi yêu cầu bảo hành</button>
-            </form>
+            </div>
         </div>
     </div>
-</div>
-<!-- Warranty End -->
+</form>
+<!-- Warranty Request End -->
 
 <?php include "./inc/footer.php"; ?>
+
+<script>
+    // JavaScript for handling form validation or other dynamic features can be added here
+</script>

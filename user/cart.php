05c8ac26827c59d89787607698474ea28a130017
Junsb11 <lt25121999@gmail.com>
2024-11-27 15:57:40 +0700

    cập nhật mới nhất

--- user/cart.php
+++ user/cart.php
@@ -4,6 +4,11 @@ if (!isset($_SESSION)) {
 }
 include '../admin/connect.php';
 
+// Kiểm tra nếu giỏ hàng chưa được khởi tạo
+if (!isset($_SESSION['cart'])) {
+    $_SESSION['cart'] = array();
+}
+
 // Lấy tham số từ GET và làm sạch
 $masp = filter_input(INPUT_GET, 'id', FILTER_SANITIZE_NUMBER_INT);
 $quantity = filter_input(INPUT_GET, 'quantity', FILTER_SANITIZE_NUMBER_INT);
@@ -18,12 +23,19 @@ if (!isset($masp) || !isset($quantity) || $quantity <= 0) {
 $stmt = $conn->prepare("SELECT * FROM san_pham WHERE MaSP = ?");
 $stmt->bind_param("i", $masp);
 $stmt->execute();
+
+// Kiểm tra nếu có lỗi trong quá trình truy vấn
+if ($stmt->error) {
+    echo "Error executing query: " . $stmt->error;
+    exit;
+}
+
 $result_sp = $stmt->get_result();
 
 // Kiểm tra nếu sản phẩm tồn tại
 if ($row_sp = $result_sp->fetch_assoc()) {
     // Cập nhật hoặc thêm sản phẩm vào giỏ hàng
-    if (!empty($_SESSION['cart'][$masp])) {
+    if (isset($_SESSION['cart'][$masp])) {
         $_SESSION['cart'][$masp]['sl'] += $quantity;
     } else {
         $_SESSION['cart'][$masp] = array(
@@ -36,9 +48,11 @@ if ($row_sp = $result_sp->fetch_assoc()) {
     }
     // Chuyển hướng đến giỏ hàng
     header("Location: listcart.php");
+    exit;
 } else {
     // Sản phẩm không tồn tại
     header("Location: chitietsp.php?id=$masp&error=product_not_found");
+    exit;
 }
 
 $stmt->close();

4235399979d5d3c1f423675094cd8c5b54d1b32a
Junsb11 <lt25121999@gmail.com>
2024-12-06 15:51:39 +0700

    cập nhật mới nhất

--- admin/themmoinhanvien.php
+++ admin/themmoinhanvien.php
@@ -1,25 +1,29 @@
-<?php include 'inc/header.php';?>
-<?php include 'inc/sidebar.php';?>
+<?php include 'inc/header.php'; ?>
+<?php include 'inc/sidebar.php'; ?>
 <?php
-    include 'connect.php';
-    if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-        $tenNV = $_POST['TenNV'];
-        $email = $_POST['Email'];
-        $soDienThoai = $_POST['SoDienThoai'];
-        $chucVu = $_POST['ChucVu'];
-        $luongCoBan = $_POST['LuongCoBan'];
-        $trangThai = $_POST['TrangThai'];
-        $ghiChu = $_POST['GhiChu'];
+include 'connect.php';
 
-        $sql_themnv = "INSERT INTO nhan_vien (TenNV, Email, SoDienThoai, ChucVu, LuongCoBan, TrangThai, GhiChu) 
-                       VALUES ('$tenNV', '$email', '$soDienThoai', '$chucVu', '$luongCoBan', '$trangThai', '$ghiChu')";
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $hoTen = $_POST['HoTen'];
+    $ngaySinh = $_POST['NgaySinh'];
+    $diaChi = $_POST['DiaChi'];
+    $ngayVaoLam = $_POST['NgayVaoLam'];
+    $luongCB = $_POST['LuongCB'];
+    $vaiTro = $_POST['VaiTro'];
+    $sdt = $_POST['SDT'];
+    $email = $_POST['Email'];
+    $trangThai = $_POST['TrangThai'];
+    $ghiChu = $_POST['GhiChu'];
 
-        if (mysqli_query($conn, $sql_themnv)) {
-            echo "<script>alert('Thêm nhân viên thành công');</script>";
-        } else {
-            echo "<script>alert('Lỗi khi thêm nhân viên');</script>";
-        }
+    $sql_themnv = "INSERT INTO nhan_vien (HoTen, NgaySinh, DiaChi, NgayVaoLam, LuongCB, VaiTro, SDT, Email, TrangThai, GhiChu) 
+                   VALUES ('$hoTen', '$ngaySinh', '$diaChi', '$ngayVaoLam', '$luongCB', '$vaiTro', '$sdt', '$email', '$trangThai', '$ghiChu')";
+
+    if (mysqli_query($conn, $sql_themnv)) {
+        echo "<script>alert('Thêm nhân viên thành công');</script>";
+    } else {
+        echo "<script>alert('Lỗi khi thêm nhân viên: " . mysqli_error($conn) . "');</script>";
     }
+}
 ?>
 <div class="grid_10">
     <div class="box round first grid">
@@ -28,24 +32,36 @@
             <form action="" method="POST">
                 <table class="form">
                     <tr>
-                        <td>Tên nhân viên</td>
-                        <td><input type="text" name="TenNV" placeholder="Nhập tên nhân viên" required /></td>
+                        <td>Họ tên</td>
+                        <td><input type="text" name="HoTen" placeholder="Nhập họ tên" required /></td>
+                    </tr>
+                    <tr>
+                        <td>Ngày sinh</td>
+                        <td><input type="date" name="NgaySinh" required /></td>
+                    </tr>
+                    <tr>
+                        <td>Địa chỉ</td>
+                        <td><input type="text" name="DiaChi" placeholder="Nhập địa chỉ" required /></td>
+                    </tr>
+                    <tr>
+                        <td>Ngày vào làm</td>
+                        <td><input type="date" name="NgayVaoLam" required /></td>
+                    </tr>
+                    <tr>
+                        <td>Lương cơ bản</td>
+                        <td><input type="number" name="LuongCB" placeholder="Nhập lương cơ bản" required /></td>
+                    </tr>
+                    <tr>
+                        <td>Vai trò</td>
+                        <td><input type="text" name="VaiTro" placeholder="Nhập vai trò" required /></td>
+                    </tr>
+                    <tr>
+                        <td>Số điện thoại</td>
+                        <td><input type="text" name="SDT" placeholder="Nhập số điện thoại" required /></td>
                     </tr>
                     <tr>
                         <td>Email</td>
                         <td><input type="email" name="Email" placeholder="Nhập email" required /></td>
-                    </tr>
-                    <tr>
-                        <td>Số điện thoại</td>
-                        <td><input type="text" name="SoDienThoai" placeholder="Nhập số điện thoại" required /></td>
-                    </tr>
-                    <tr>
-                        <td>Chức vụ</td>
-                        <td><input type="text" name="ChucVu" placeholder="Nhập chức vụ" required /></td>
-                    </tr>
-                    <tr>
-                        <td>Lương cơ bản</td>
-                        <td><input type="number" name="LuongCoBan" placeholder="Nhập lương cơ bản" required /></td>
                     </tr>
                     <tr>
                         <td>Trạng thái</td>
@@ -69,4 +85,4 @@
         </div>
     </div>
 </div>
-<?php include 'inc/footer.php';?>
+<?php include 'inc/footer.php'; ?>
